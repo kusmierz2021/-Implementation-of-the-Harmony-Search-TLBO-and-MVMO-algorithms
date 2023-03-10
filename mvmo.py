8e089ec0324b572dde3bf021a7a329cc62c2e62a
Rafał Kuśmierz <r.kusmierz@stud.elka.pw.edu.pl>
2023-03-10 13:32:47 +0100

    (de)normalize population tests added

--- mvmo.py
+++ mvmo.py
@@ -2,7 +2,8 @@ import numpy as np
 
 
 class MVMO:
-    def __init__(self, iterations: int, population_size: int, dimensions: int, num_mutation: int, shaping_scaling_factor_fs=1.0, asymmetry_factor_af=1.0, init_val_shape_factor_sd=75.0):
+    def __init__(self, iterations: int, population_size: int, dimensions: int, num_mutation: int,
+                 shaping_scaling_factor_fs=1.0, asymmetry_factor_af=1.0, init_val_shape_factor_sd=75.0):
         self.iterations = iterations
         self.population_size = population_size
         self.dimensions = dimensions
@@ -11,10 +12,8 @@ class MVMO:
         self.asymmetry_factor_af = asymmetry_factor_af
         self.init_val_shape_factor_sd = init_val_shape_factor_sd
 
-
     def optimize(self, boundaries: tuple[int, int]):
         pass
-
 
     def init_population(self, dim: int, size: int = 2, low=0.0, high=1.0) -> list[np.ndarray]:
         """
@@ -27,7 +26,6 @@ class MVMO:
         """
         return [np.random.uniform(low=low, high=high, size=(dim,)) for _ in range(size)]
 
-
     def normalize_population(self, population: list[np.ndarray], low, high):
         """
         Normalizes the input population
@@ -38,8 +36,7 @@ class MVMO:
         """
         return [(ind - low) / (high - low) for ind in population]
 
-
-    def denormalize_population(population: list[np.ndarray], low, high):
+    def denormalize_population(self, population: list[np.ndarray], low, high):
         """
         Denormalizes the input population
         :param population: population to denormalize
@@ -50,8 +47,5 @@ class MVMO:
         return [(ind * (high - low)) + low for ind in population]
 
 
-
 if __name__ == '__main__':
     optimizer = MVMO(10000, 100, 5, 3)
-
-
--- mvmo_test.py
+++ mvmo_test.py
@@ -1,10 +1,11 @@
+import numpy as np
 from mvmo import MVMO
 
 
 def test_init_population():
     optimizer = MVMO(10000, 100, 5, 3)
     population_1 = optimizer.init_population(3, 3)
-    population_2 = optimizer.init_population(2, 10, 0.12, 5.12)
+    population_2 = optimizer.init_population(2, 10, -5.12, 5.12)
 
     assert len(population_1) == 3
     assert len(population_1[0]) == 3
@@ -16,3 +17,14 @@ def test_init_population():
     assert len(population_2[0]) == 2
     assert -5.12 <= population_2[0][0] <= 5.12
     assert -5.12 <= population_2[1][1] <= 5.12
+
+
+def test__de_normalize_population():
+    optimizer = MVMO(10000, 100, 5, 3)
+    population = optimizer.init_population(2, 10, -5.12, 5.12)
+    normalized_population = optimizer.normalize_population(population, -5.12, 5.12)
+    denormalized_population = optimizer.denormalize_population(normalized_population, -5.12, 5.12)
+
+    assert np.array_equal(denormalized_population, population)
+    assert max([ind.max() for ind in normalized_population]) <= 1.0
+    assert min([ind.min() for ind in normalized_population]) >= 0.0
