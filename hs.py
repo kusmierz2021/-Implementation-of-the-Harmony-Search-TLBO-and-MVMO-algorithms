d04055a95151f16f12a37dd672ad661c8aaeeea5
Rafał Kuśmierz <r.kusmierz@stud.elka.pw.edu.pl>
2023-03-13 10:48:17 +0100

    evaluation and base reproduction added

--- hs.py
+++ hs.py
@@ -1,6 +1,9 @@
 from evolutionary_algorithm import EvolutionaryAlgorithm
 import numpy as np
 from numpy.random import randint
+from optimization_functions import rastrigins_function
+
+
 class HS(EvolutionaryAlgorithm):
     def __init__(self, iterations: int, dimensions: int, boundaries: tuple[float, float], hmcr: float = None,
                  par: float = None):
@@ -8,8 +11,10 @@ class HS(EvolutionaryAlgorithm):
         self.hmcr = hmcr
         self.par = par
 
-    def evaluation(self, population: list[np.ndarray], fitness_function: callable):
-        pass
+    def evaluation(self, population: list[np.ndarray], fitness_function: callable, child: np.ndarray):
+        population = population + [child]
+        best_population = sorted([(ind, fitness_function(ind)) for ind in population], key=lambda ind: ind[1], reverse=True).copy()[:len(population) - 1]
+        return best_population
 
     def reproduction(self, population: list[np.ndarray]) -> np.ndarray:
         child = np.empty(self.dimensions, dtype=float)
@@ -17,15 +22,24 @@ class HS(EvolutionaryAlgorithm):
             child[ind] = population[randint(0, len(population))][ind]
         return child
 
+    def optimize(self, population: list[np.ndarray]):
+
+        pass
 
 if __name__ == '__main__':
     boundaries = (-5.12, 5.12)
     optimizer = HS(10000, 6, boundaries)
-    population = optimizer.init_population(500)
+    population = optimizer.init_population(5)
     child = optimizer.reproduction(population)
     for ind in population:
         print(ind)
 
-    print('---->\n')
+    print('\n')
+    print('\n')
     print(child)
+    print('\n')
+    print('\n')
+    population = optimizer.evaluation(population, rastrigins_function, child)
+    for ind in population:
+        print(ind)
 
--- hs_test.py
+++ hs_test.py
@@ -2,12 +2,6 @@ from hs import HS
 from optimization_functions import rastrigins_function
 import numpy as np
 
-    # def reproduction(self, population: list[np.ndarray]) -> np.ndarray:
-    #     offspring = np.empty(self.dimensions, dtype=float)
-    #     for ind in range(self.dimensions):
-    #         offspring[ind] = population[randint(0, len(population))][ind]
-    #     return offspring
-
 def test_reproduction():
     dimensions = 6
     boundaries = (-5.12, 5.12)
@@ -18,4 +12,11 @@ def test_reproduction():
     assert len(child) == dimensions
     assert all(boundaries[0] <= gene <= boundaries[1] for gene in child)
 
+def test_evaluation():
+    boundaries = (-5.12, 5.12)
+    optimizer = HS(10000, 6, boundaries)
+    population = optimizer.init_population(5)
+    child = optimizer.reproduction(population)
+    evaluated_population = optimizer.evaluation(population, rastrigins_function, child)
 
+    assert len(evaluated_population) ==len(population)
