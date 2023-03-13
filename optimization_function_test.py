537ecbc117de14cf5a8e2bf40a01e574684948ed
Rafał Kuśmierz <r.kusmierz@stud.elka.pw.edu.pl>
2023-03-13 17:00:35 +0100

    zakharov optimization function added

--- optimization_function_test.py
+++ optimization_function_test.py
@@ -1,8 +1,60 @@
 import optimization_functions
 import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib as mpl
 
 
 def test_rastrigins_function():
     assert round(optimization_functions.rastrigins_function(np.array([4.52299])), 5) == 40.35329
     assert round(optimization_functions.rastrigins_function(np.array([4.52299, 4.52299])), 5) == 80.70658
     assert round(optimization_functions.rastrigins_function(np.array([4.52299, 4.52299, 4.52299])), 5) == 121.05987
+    assert round(optimization_functions.rastrigins_function(np.array([4.52299, 4.52299, 4.52299,
+                                                                      4.52299])), 5) == 161.41316
+    assert round(optimization_functions.rastrigins_function(np.array([4.52299, 4.52299, 4.52299,
+                                                                      4.52299, 4.52299])), 5) == 201.76645
+    assert round(optimization_functions.rastrigins_function(np.array([4.52299, 4.52299, 4.52299,
+                                                                      4.52299, 4.52299, 4.52299])), 5) == 242.11974
+    assert round(optimization_functions.rastrigins_function(np.array([4.52299, 4.52299, 4.52299, 4.52299,
+                                                                      4.52299, 4.52299, 4.52299])), 5) == 282.47303
+
+    # plot rastrigins function (2D)
+    # mpl.use('TkAgg')
+    # x = np.linspace(-5.12, 5.12, 1000)
+    # y = np.linspace(-5.12, 5.12, 1000)
+    # x, y = np.meshgrid(x, y)
+    # z = np.array([optimization_functions.rastrigins_function(np.array([x, y])) for x, y in zip(x, y)])
+    #
+    # ax = plt.axes(projection="3d")
+    # ax.plot_surface(x, y, z, cmap="viridis")
+    # ax.set_xlabel('x')
+    # ax.set_ylabel('y')
+    # ax.set_zlabel('f(x, y)')
+    # ax.view_init(10, 10)
+    # plt.title("Rastrigin's function (2D)")
+    # plt.show()
+
+
+def test_zakharov_function():
+
+    assert round(optimization_functions.zakharov_function(np.array([0, 0, 0, 0, 0, 0])), 2) == 0
+    assert round(optimization_functions.zakharov_function(np.array([0, 0, 0, 0, 0])), 2) == 0
+    assert round(optimization_functions.zakharov_function(np.array([0, 0, 0, 0])), 2) == 0
+    assert round(optimization_functions.zakharov_function(np.array([0, 0, 0])), 2) == 0
+    assert round(optimization_functions.zakharov_function(np.array([0, 0])), 2) == 0
+    assert round(optimization_functions.zakharov_function(np.array([0])), 2) == 0
+
+    # plot zakharov function (2D)
+    # mpl.use('TkAgg')
+    # x = np.linspace(-5, 10, 1000)
+    # y = np.linspace(-5, 10, 1000)
+    # x, y = np.meshgrid(x, y)
+    # z = np.array([optimization_functions.zakharov_function(np.array([x, y])) for x, y in zip(x, y)])
+    #
+    # ax = plt.axes(projection="3d")
+    # ax.plot_surface(x, y, z, cmap="viridis")
+    # ax.set_xlabel('x')
+    # ax.set_ylabel('y')
+    # ax.set_zlabel('f(x, y)')
+    # ax.view_init(10, 10)
+    # plt.title('Zakharov function (2D)')
+    # plt.show()
--- optimization_functions.py
+++ optimization_functions.py
@@ -1,10 +1,21 @@
 import math
-import numpy
+import numpy as np
 
 
-def rastrigins_function(vec: numpy.ndarray) -> float:
+def rastrigins_function(vec: np.ndarray) -> float:
     """
     :param vec: real numbers vector, includes numbers from [-5,12, 5,12]
     :return: value of function
     """
-    return sum([x**2 - 10 * math.cos(2 * math.pi * x) + 10 for x in vec])
+    return sum([x**2 - 10 * np.cos(2 * math.pi * x) + 10 for x in vec])
+
+
+def zakharov_function(vec: np.ndarray) -> float:
+    """
+
+    :param vec: real numbers vector, usually includes numbers from [-5, 10]
+    :type vec: numpy.ndarray
+    :return: value of function
+    :rtype: float
+    """
+    return sum([x**2 for x in vec]) + sum([0.5 * x for x in vec])**2 + sum([0.5 * x for x in vec])**4
